42e791e2bbfba171654584ccfae5295a35bb1554
Chanchhay Srey <chanchhay07@gmail.com>
2025-07-23 11:42:55 +0700

    updated style

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,9 @@ document.getElementById("userForm").addEventListener("submit", function (event) 
   event.preventDefault();
 
   const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
   const email = document.getElementById("email").value;
+  const position = document.getElementById("position").value;
   const fileInput = document.getElementById("profilePic");
   const file = fileInput.files[0];
 
@@ -10,7 +12,9 @@ document.getElementById("userForm").addEventListener("submit", function (event) 
     const imageURL = URL.createObjectURL(file);
 
     document.getElementById("displayName").innerText = name;
+    document.getElementById("displayAge").innerText = age + " years old";
     document.getElementById("displayEmail").innerText = email;
+    document.getElementById("displayPosition").innerText = position;
     document.getElementById("displayImage").src = imageURL;
 
     document.getElementById("userInfo").style.display = "block";
